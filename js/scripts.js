4d12eca13cdcf3bba7d7ceddf9d02a57fc1dd6d9
kathrynceciliatrahan@gmail.com and ryanglenferris@gmail.com <kathrynceciliatrahan@gmail.com@>
2017-12-04 11:29:58 -0800

    consolidate logic functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,28 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput = $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var verbInput = $("input#verb").val();
+    // var nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamtion").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    var inputs = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+    debugger;
+
+    inputs.forEach(function(input) {
+      debugger;
+      var userInput = $("#"+input).val();
+      $("."+input).text(userInput);
+    });
+
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamtion").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
 
     $("#story").show();
 
